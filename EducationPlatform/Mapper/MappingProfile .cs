250fcbbcf3e249544734e8e175359b0a5f82c2bd
Eid Sonkrot <Eid Sonkrot@EID-SONKROT>
2024-08-18 10:58:39 +0300

    mapper

---
+++ EducationPlatform/Mapper/MappingProfile .cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Domain.Models;
+using EducationPlatform.DTO;
+
+namespace EducationPlatform.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<TenantVerificationDTO, TenantVerification>();
+            CreateMap<TenantVerification, TenantVerificationDTO>();
+            CreateMap<SlideMakerDto, Slide>();
+            CreateMap<VideoCreatorDto, Video>();
+            CreateMap<DesignToolDto, Design>();
+            CreateMap<AutoGradeDto, AssignmentGrade>();
+        }
+    }
+}
