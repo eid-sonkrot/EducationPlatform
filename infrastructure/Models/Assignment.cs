43c222bb373dd36839e9a2250611b2eecf349d52
Eid Sonkrot <Eid Sonkrot@EID-SONKROT>
2024-08-18 10:56:29 +0300

    ccc

---
+++ infrastructure/Models/Assignment.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace infrastructure.Models
+{
+    public class Assignment
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string AssignmentName { get; set; }
+
+        public List<Question> Questions { get; set; }
+
+        public Guid TeacherId { get; set; }
+
+        public Guid TenantId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
