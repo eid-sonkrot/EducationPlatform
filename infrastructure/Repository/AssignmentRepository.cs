938c4ebb988e225dac616916c0318cb2883d8b7e
Eid Sonkrot <Eid Sonkrot@EID-SONKROT>
2024-08-18 10:56:47 +0300

    cc

---
+++ infrastructure/Repository/AssignmentRepository.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Doamin.IRepository;
+using Domain.Models;
+using MongoDB.Driver;
+using System.Linq.Expressions;
+
+namespace infrastructure.Repository
+{
+    public class AssignmentRepository : MongoRepository<infrastructure.Models.Assignment>,IAssignmentRepository
+    {
+        private readonly IMapper _mapper;
+        public AssignmentRepository(IMongoDatabase database, IMapper mapper) : base(database, "Assignments")
+        {
+            _mapper = mapper;
+        }
+
+        public Task AddAsync(Assignment entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Assignment>> FindAsync(Expression<Func<Assignment, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Assignment> GetAssignmentByNameAsync(string assignmentName)
+        {
+            var filter = Builders<infrastructure.Models.Assignment>.Filter.Eq(a => a.AssignmentName, assignmentName);
+            var AssignmentData = await _collection.Find(filter).FirstOrDefaultAsync();
+            return _mapper.Map<Assignment>(AssignmentData);
+        }
+
+        public async Task UpdateAsync(Guid id, Assignment entity)
+        {
+
+            var filter = Builders<infrastructure.Models.Assignment>.Filter.Eq(a => a.Id, id.ToString());
+            var update = Builders<infrastructure.Models.Assignment>.Update
+                .Set(a => a.AssignmentName, entity.AssignmentName)
+                .Set(a => a.Questions, entity.Questions)
+                .Set(a => a.TeacherId, entity.TeacherId)
+                .Set(a => a.TenantId, entity.TenantId)
+                .Set(a => a.UpdatedAt, DateTime.UtcNow);
+
+            await _collection.UpdateOneAsync(filter, update);
+        }
+
+        Task<IEnumerable<Assignment>> IRepository<Assignment>.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Assignment> IRepository<Assignment>.GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ infrastructure/Repository/MongoRepository.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Driver;
+using System.Linq.Expressions;
+
+namespace infrastructure.Repository
+{
+    public class MongoRepository<T>
+    {
+        protected readonly IMongoCollection<T> _collection;
+
+        public MongoRepository(IMongoDatabase database, string collectionName)
+        {
+            _collection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _collection.Find(Builders<T>.Filter.Empty).ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            return await _collection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _collection.Find(filter).ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _collection.InsertOneAsync(entity);
+        }
+
+        public async Task UpdateAsync(Guid id, T entity)
+        {
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            await _collection.ReplaceOneAsync(filter, entity);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            await _collection.DeleteOneAsync(filter);
+        }
+    }
+}
