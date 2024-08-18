19fb58df9191f188c70ea063c36d863cfbea72c1
Eid Sonkrot <Eid Sonkrot@EID-SONKROT>
2024-08-18 10:58:25 +0300

    ident

---
+++ EducationPlatform/Identity/ApplicationDbContext .cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationPlatform.Identity
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // You can override methods here if needed, for example, to customize the model building.
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names here.
+        }
+    }
+}
---
+++ EducationPlatform/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+namespace EducationPlatform.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public Guid TenantId { get; set; }
+    }
+}
