76470c2c6a0323760bd96753ba666d155f98ae33
Eid Sonkrot <Eid Sonkrot@EID-SONKROT>
2024-08-18 10:55:54 +0300

    CCC

---
+++ Domain/IRepository/IAssignmentRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+
+namespace Doamin.IRepository
+{
+    public interface IAssignmentRepository : IRepository<Assignment>
+    {
+        Task<Assignment> GetAssignmentByNameAsync(string assignmentName);
+    }
+}
---
+++ Domain/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Doamin.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> filter);
+        Task AddAsync(T entity);
+        Task UpdateAsync(Guid id, T entity);
+        Task DeleteAsync(Guid id);
+    }
+}
